5237e1d807b19a65721d866b40ac8e8dd1e59e80
Divyesh Bhatt <divyesh.bhatt@differenzsystem.com>
2021-04-02 11:23:17 +0530

    Merged changes

--- NewControlsDemo/ViewModels/WebUrlTestPageViewModel.cs
+++ NewControlsDemo/ViewModels/WebUrlTestPageViewModel.cs
@@ -9,7 +9,8 @@ namespace NewControlsDemo.ViewModels
         public WebUrlTestPageViewModel(INavigationService navigationService, FacadeService facadeService)
              : base(navigationService, facadeService)
         {
-            AccountPageURL = "http://54.244.107.179/Plaid/Plaid"; //"http://34.216.71.66/Plaid/Plaid";
+            //AccountPageURL = "http://54.244.107.179/Plaid/Plaid"; //"http://34.216.71.66/Plaid/Plaid";
+            AccountPageURL = "https://us04web.zoom.us/j/9218226818?pwd=ZmdXcjJ3Z2RqcUE4UzBmVnhwWUNsdz09";
         }
 
         private string _accountPageURL;
